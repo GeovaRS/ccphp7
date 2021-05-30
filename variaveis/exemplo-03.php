52f8bd1653ae6cd0dc0f1c84ff5c8a148ebbc404
GeovaRS <gevsantos@hotmail.com>
2021-05-29 23:25:31 -0300

    added files exemplos-02/03/04.php

---
+++ variaveis/exemplo-03.php
@@ -0,0 +1,28 @@
+<?php
+ // $nome = "HCode";
+ // $site = 'www.hcode.com.br';
+ // $ano = 1990;
+ // $salario = 5500.99;
+ // $bloqueado = false;
+
+ // Tipos Compostos Arrays e Objetos
+
+ // $frutas = array("abacaxi", "laranja", "manga", "banana", "maçã");
+
+ // echo $frutas[2];
+
+ // $nascimento = new DateTime();
+
+ // var_dump($nascimento);
+
+ // Tipos de Dados Especiais Resource, null e empty
+
+ // $arquivo = fopen("exemplo-03.php", "r");
+ // var_dump($arquivo);
+
+ // Ausência de Valor
+ // $nulo = NULL;
+
+ // Vazio - Existe em espaço reservado de memória, porém sem valor.
+ // $vazio = "";
+?>
