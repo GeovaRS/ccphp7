863d41dad4cddf9b75b1b5f8a669c0e7a928d942
GeovaRS <gevsantos@hotmail.com>
2021-06-01 22:20:44 -0300

    s005 a003 constante added

---
+++ constante/exemplo-02.php
@@ -0,0 +1,12 @@
+<?php
+ define("BANCO_DE_DADOS", [
+  '127.0.0.1',
+  'root',
+  'password',
+  'teste'
+ ], true);
+ print_r(BANCO_DE_DADOS);
+
+ // Constantes Case Insensitive usar o terceiro parâmetro como true
+ // Não usual
+?>
---
+++ constante/exemplo-03.php
@@ -0,0 +1,11 @@
+<?php
+ echo PHP_VERSION;
+ echo "<br />";
+
+ // Separador de Diretórios/Pastas nos Sistemas Operacionais
+ // Microsoft Windows (\)
+ // Linux (/)
+ echo DIRECTORY_SEPARATOR;
+
+ // https://secure.php.net/manual/pt_BR/reserved.constants.php
+?>
