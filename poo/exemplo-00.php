4e60617ae03d348276f16c3f3ef6b384e27041e9
GeovaRS <gevsantos@hotmail.com>
2021-06-04 16:50:19 -0300

    s009 a001 poo added

---
+++ poo/exemplo-00.php
@@ -0,0 +1,11 @@
+<?php
+ // POO - Programação Orientada a Objetos
+ // POO é um Paradigma Conceitual de Programação
+ // Algumas Linguagens de Programação são Totalmente Orientada a Objetos
+ // Classes
+ // Modelo/Molde com Definições de Variáveis(Atributos) e Funções(Métodos)
+ // Atributos - Armazenam Informações/Dados
+ // Métodos - Executam Ações
+ // Objetos
+ // Variáveis que criam/representam Classes quando Instânciadas.
+?>
