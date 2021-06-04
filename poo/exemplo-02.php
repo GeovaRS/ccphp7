b5088d426a152b98ec84c694beac50cbacb2ae6e
GeovaRS <gevsantos@hotmail.com>
2021-06-04 17:54:04 -0300

    s009 a003 poo example-02.php added

---
+++ poo/exemplo-02.php
@@ -0,0 +1,46 @@
+<?php
+ class carro {
+  private $modelo;
+  private $motor;
+  private $ano;
+
+  public function getModelo(): string {
+   return $this->modelo;
+  }
+
+  public function setModelo($modelo) {
+   $this->modelo = $modelo;
+  }
+
+  public function getMotor(): float {
+   return $this->motor;
+  }
+
+  public function setMotor($motor) {
+   $this->motor = $motor;
+  }
+
+  public function getAno(): int {
+   return $this->ano;
+  }
+
+  public function setAno($ano) {
+   $this->ano = $ano;
+  }
+
+  public function exibir() {
+   return array(
+    "modelo"=>$this->getModelo(),
+    "motor"=>$this->getMotor(),
+    "ano"=>$this->getAno()
+   );
+  }
+ }
+
+ $gol = new Carro();
+ $gol->setModelo("Gol GT");
+ $gol->setMotor("1.6");
+ $gol->setAno("2016");
+ print_r($gol->exibir());
+ var_dump($gol->exibir());
+?>
