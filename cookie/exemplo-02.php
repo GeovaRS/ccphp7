3c9c2ffee8b4faa8014f15fff061dc8254963c85
GeovaRS <gevsantos@hotmail.com>
2021-06-10 19:40:43 -0300

    s014 a009 cookie added

---
+++ cookie/exemplo-02.php
@@ -0,0 +1,8 @@
+<?php
+ if(isset($_COOKIE["NOME_DO_COOKIE"])) {
+  // var_dump(json_decode($_COOKIE["NOME_DO_COOKIE"]));
+  $obj = json_decode($_COOKIE["NOME_DO_COOKIE"]);
+  // echo $obj->empresa;
+  echo $obj->empresa;
+ }
+?>
